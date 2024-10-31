23b083af08daa335e97cb0f4bb653152103334af
saneknapenek <AlEkSaNdR.KoLeSnIkOv22@yandex.ru>
2024-10-31 12:51:07 +0300

    added search hidden files

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -1,2 +1,47 @@
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<C-M-p>', builtin.find_files, { desc = 'Telescope find files' })
+
+require('telescope').setup({
+	defaults = {
+		hidden = true,
+		vimgrep_arguments = {
+			"rg",
+			"--color=never",
+    		"--follow",
+    		"--hidden",
+    		"--no-heading",
+    		"--with-filename",
+    		"--line-number",
+    		"--column",
+    		"--smart-case",
+			"--hidden",
+
+			"--glob=!**/.git/*",
+			"--glob=!**/.idea/*",
+      		"--glob=!**/.vscode/*",
+      		"--glob=!**/build/*",
+      		"--glob=!**/dist/*",
+      		"--glob=!**/yarn.lock",
+      		"--glob=!**/package-lock.json",
+      		"--glob=!**/venv/*",
+		},
+	},
+	pickers = {
+		find_files = {
+			hidden = true,
+			find_command = {
+				"rg",
+				"--files",
+				"--hidden",
+				"--glob=!**/.git/*",
+				"--glob=!**/.idea/*",
+				"--glob=!**/.vscode/*",
+				"--glob=!**/build/*",
+				"--glob=!**/dist/*",
+				"--glob=!**/yarn.lock",
+				"--glob=!**/package-lock.json",
+				"--glob=!**/venv/*",
+			},
+		},
+	},
+})
