0567051183266f7c782ac24195a0a2c4a247fb3d
saneknapenek <AlEkSaNdR.KoLeSnIkOv22@yandex.ru>
2024-11-05 00:32:44 +0300

    fixed audi_sinks script

--- rofi/audio_sinks/exec.sh
+++ rofi/audio_sinks/exec.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
 
-output_devices=$(wpctl status | awk '/^ ├─ Sinks:/{flag=1; next} /^ ├─ Sources:/{flag=0} flag && NF {print $2, $3, $4, $5}')
+output_devices=$(wpctl status | sed -n '/^ ├─ Sinks:/,/^ ├─ Sources:/p' | awk '/[0-9]+\./ {print $2, substr($0, index($0,$3))}')
 
 if [ -z "$output_devices" ]; then
-    echo "Нет доступных устройств вывода"
+    notify-send "Нет доступных устройств вывода"
     exit 1
 fi
 
-selected_device=$(echo "$output_devices" | rofi -dmenu -config ~/.config/rofi/audio_sinks/config.rasi)
+num_lines=$(echo "$output_devices" | wc -l)
+num_lines=$(( num_lines > 6 ? 6 : num_lines ))
+
+selected_device=$(echo "$output_devices" | rofi -dmenu -l $num_lines -config ~/.config/rofi/audio_sinks/config.rasi)
 
 if [ -n "$selected_device" ]; then
     wpctl set-default "$selected_device"
     notify-send "Устройство вывода установлено на: $selected_device"
-else
-    echo "Выбор устройства отменен."
 fi
 
