2b14c487951729a5deb86732a9c8824126a3ee6c
saneknapenek <AlEkSaNdR.KoLeSnIkOv22@yandex.ru>
2025-02-03 23:55:18 +0300

    change binded key for nvim tagbar plugin

--- nvim/lua/plugins/tagbar.lua
+++ nvim/lua/plugins/tagbar.lua
@@ -1,4 +1,4 @@
-vim.keymap.set('n', '<C-S-o>', ':TagbarToggle [f]<CR>', { desc = 'Tagbar toggle' })
+vim.keymap.set('n', '<C-T>', ':TagbarToggle [f]<CR>', { desc = 'Tagbar toggle' })
 
 -- Привязка клавиш для Tagbar
 -- vim.api.nvim_set_keymap('n', '<C-k>', ':call tagbar#jumpToNearbyTag(1)<CR>', { noremap = true, silent = true })
